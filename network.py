48bf3d46c34e38b62f70fee5ea1915d1ed05d56e
Maciej Rybarczyk <maciej.rybarczyk@onet.pl>
2025-01-10 20:49:04 +0100

    Add train and predict methods to Network class

--- network.py
+++ network.py
@@ -1,8 +1,9 @@
+import numpy as np
 from layer import Layer
 
 """
     This class is used to initialize a neural network
-    with given number of layers
+    with given number of layers and train it
 """
 
 class Network():
@@ -14,3 +15,38 @@ class Network():
         # Initialize layers
         for i in range (len(layers) - 1):
             self.layers.append(Layer(layers[i+1], layers[i]))
+        
+    def train(self, inputs, outputs):
+        for epoch in range(self.epochs):
+            total_error = 0
+
+            for x, y in zip(inputs, outputs):
+                # Forward pass
+                activations = [x]
+
+                for layer in self.layers:
+                    activations.append(layer.forward(activations[-1]))
+                
+                # Calculate error
+                output_errors = y - activations[-1]
+                total_error += np.sum(output_errors ** 2)
+
+                # Backward pass
+                errors = output_errors
+                
+                for i in reversed(range(len(self.layers))):
+                    errors = self.layers[i].backward(errors, self.learning_rate)
+
+            if epoch % 100 == 0:
+                mse = total_error / len(inputs)
+                print(f'Epoch {epoch}, MSE: {mse}')
+
+    # The purpose of this method is to use the model, that
+    # has been trained, on new data
+    def predict(self, inputs):
+        activations = inputs
+
+        for layer in self.layers:
+            activations = layer.forward(activations)
+        
+        return activations
